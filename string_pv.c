9ccacc196acb537bcced2819479434aaba964d62
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-05-04 19:08:06 -0300

    Create string_pv.c

---
+++ string_pv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char *al= "conjunto"; //ponteiro para uma string constante "conjunto"
+    char re[]="simples"; // array de caracteres, uma string que pode ser modificada
+    puts(al); // imprimi "conjunto"
+    puts (&re[0]); // ou puts(re), imprimi "simples"
+    for(;*(al+1); al++); //loop até último caracter antes do '\0'
+    putchar(*al); // imprimi '\0' que não é visivel
+    putchar('\n'); // quebra de linha
+    return 0;
+}
